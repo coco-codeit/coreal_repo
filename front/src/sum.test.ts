be642567ac53b77c1f351a90161ae32b944bdb68
Daniel LEE <dmsgur7112@gmail.com>
2024-09-09 14:51:31 +0900

    Chore: lint-staged 설치 staged area에 올라간 파일들만 lint가 실행되도록 설정

--- front/src/sum.test.ts
+++ front/src/sum.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
-import { add } from "./add";
+import { sum } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);
 });
