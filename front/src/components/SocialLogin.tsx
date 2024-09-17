591ab38c3038809b0353f66707d663e65d0f71b0
Jungmin <miniharu.zip@gmail.com>
2024-09-17 20:23:41 +0900

    Feat: 로그인 유효성 검사 추가
    - Zod를 사용한 이메일 및 비밀번호 유효성 검사
    - axios로 로그인 API 호출 처리
    - 입력값 오류 메시지 및 제출 중 로딩 상태 반영

--- front/src/components/SocialLogin.tsx
+++ front/src/components/SocialLogin.tsx
@@ -16,7 +16,7 @@ export default function SocialLogin() {
     <nav aria-label="소셜 미디어 로그인 옵션">
       <ul className="flex justify-center gap-x-4">
         {SOCIAL_LOGIN_OPTIONS.map(({ url, icon, alt }) => (
-          <li key={icon}>
+          <li key={alt}>
             <Link href={url} aria-label={alt}>
               <div className="relative w-[36px] sm:w-[54px] h-[36px] sm:h-[54px] cursor-pointer">
                 <Image src={icon} alt={alt} fill />
