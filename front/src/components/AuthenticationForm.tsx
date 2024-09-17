9c8f2b4baa196931d7eb420bcde7e18c6d658c84
Jungmin <miniharu.zip@gmail.com>
2024-09-17 15:28:22 +0900

    Refactor: 로그인 UI 리팩토링
    - 로그인 양식 radius 크기 추가 수정
    - OAuth 추후 수정할 부분 TOOD 주석
    - Unique key 수정

--- front/src/components/AuthenticationForm.tsx
+++ front/src/components/AuthenticationForm.tsx
@@ -5,7 +5,7 @@ import SocialLogin from "./SocialLogin";
 export default function AuthenticationForm() {
   return (
     <section aria-labelledby="login-heading">
-      <div className="w-full mx-auto max-w-[343px] sm:max-w-[510px] bg-white rounded-3xl px-8 py-6 sm:px-14 sm:py-8 md:mx-[102px]">
+      <div className="w-full mx-auto max-w-[343px] sm:max-w-[510px] bg-white rounded-2xl sm:rounded-3xl px-8 py-6 sm:px-14 sm:py-8 md:mx-[102px]">
         <h2
           id="login-heading"
           className="text-base sm:text-display-3 md:text-display-1 font-bold pb-8 text-center"
