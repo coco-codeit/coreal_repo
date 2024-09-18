5956b3bf84a83134ff3919a74af14316d1760407
Jungmin <miniharu.zip@gmail.com>
2024-09-18 15:11:19 +0900

    Refactor: 로그인/회원가입에 따른 UI
    - 텍스트와 링크 경로를 title 변수를 사용해 간결하게 처리

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -2,10 +2,12 @@ import AuthenticationForm from "@/components/AuthenticationForm";
 import WelcomeMessage from "@/components/WelcomeMessage";
 
 export default function LoginPage() {
+  const title = "로그인";
+
   return (
     <section className="flex flex-col md:flex-row justify-center bg-gray-2 text-gray-15 pt-8 mb-8 md:pt-52">
       <WelcomeMessage />
-      <AuthenticationForm />
+      <AuthenticationForm title={title} />
     </section>
   );
 }
