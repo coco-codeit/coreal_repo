223aaaf9117c95c5d94e2d6bce2aa19a707b6b4d
Jungmin <miniharu.zip@gmail.com>
2024-09-19 16:17:44 +0900

    fix: 수정

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -2,6 +2,7 @@ import AuthenticationForm from "@/components/AuthenticationForm";
 import WelcomeMessage from "@/components/WelcomeMessage";
 
 export default function LoginPage() {
+  // TODO: 로그인, 회원가입 contant.ts 파일에 정리하기
   const title = "로그인";
 
   return (
