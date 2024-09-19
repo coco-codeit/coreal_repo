8fde134a96593f4a3e3fccb2d8a12fe7b9eb159c
황호선 <3262398@naver.com>
2024-09-19 15:17:26 +0900

    Feat: 드롭다운 구현 및 스터디/프로젝트 구분 기능 추가
    - 스터디와 프로젝트 구분하는 드롭다운 기능 구현
    - 현재는 임시데이터로 구현, applied와 created의 List 컴포넌트에서 구분된 데이터 렌더링
    - Card 컴포넌트를 생성하여 각 항목 출력

--- front/src/app/gathers/manage/page.tsx
+++ front/src/app/gathers/manage/page.tsx
@@ -1,15 +1,10 @@
-import { GatheringTabs } from "@/app/components/manage/Tabs";
-import AppliedGatherings from "@/app/components/manage/contents/AppliedGatherings";
-import CreatedGatherings from "@/app/components/manage/contents/CreatedGatherings";
+import { GatheringTabs } from "./components/manage/Tabs";
 
 function ManageGatheringsPage() {
   return (
     <div className="flex justify-center items-center min-h-screen p-20">
       <div className="w-[1200px] h-screen">
-        <GatheringTabs>
-          <AppliedGatherings />
-          <CreatedGatherings />
-        </GatheringTabs>
+        <GatheringTabs />
       </div>
     </div>
   );
