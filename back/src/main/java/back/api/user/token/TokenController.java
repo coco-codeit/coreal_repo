6652bb267f9a0f3cc23b3905a2a1773ee7c0b2cf
JEONG <qheogus55@naver.com>
2024-09-21 06:51:55 +0900

    refactor: login api
    - refresh token 쿠키 값으로 넘김(http only)

--- back/src/main/java/back/api/user/token/TokenController.java
+++ back/src/main/java/back/api/user/token/TokenController.java
@@ -3,8 +3,11 @@ package back.api.user.token;
 import back.api.common.dto.CustomApiResponse;
 import back.api.user.token.dto.TokenRequest;
 import back.api.user.token.dto.TokenResponse;
+import back.common.util.CookieUtil;
 import back.domain.user.token.RefreshTokenService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,9 @@ public class TokenController {
     private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/re-issue")
-    public CustomApiResponse<TokenResponse.ReIssue> reIssue(@RequestBody TokenRequest.ReIssue request) {
-        return CustomApiResponse.ok(refreshTokenService.reIssue(request.toCommand()));
+    public CustomApiResponse<TokenResponse.ReIssue> reIssue(@RequestBody TokenRequest.ReIssue request, HttpServletResponse response) {
+        TokenResponse.ReIssue tokenResponse = refreshTokenService.reIssue(request.toCommand());
+        CookieUtil.addRefreshTokenCookie(response,tokenResponse.getRefreshToken());
+        return CustomApiResponse.ok(tokenResponse);
     }
 }
