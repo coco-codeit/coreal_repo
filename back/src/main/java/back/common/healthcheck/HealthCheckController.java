298adc69c26245552f06b1e497bf9e683f1e0611
JEONG <qheogus55@naver.com>
2024-09-20 12:10:45 +0900

    feat: ci/cd test

--- back/src/main/java/back/common/healthcheck/HealthCheckController.java
+++ back/src/main/java/back/common/healthcheck/HealthCheckController.java
@@ -27,6 +27,7 @@ public class HealthCheckController {
 
     @GetMapping("/health-check")
     public ResponseEntity<?> healthCheck() {
+        log.info("healthCheck");
         Map<String, String> responseData = new HashMap<>();
         responseData.put("serverName", serverName);
         responseData.put("serverAddress", serverAddress);
