2650c200bbd49faf0010279312a3e0b6b7ccb5b6
Daniel LEE <dmsgur7112@gmail.com>
2024-09-23 18:03:55 +0900

    Refactor: 유저페이지 리팩토링
    - 유저 데이터 page단에서 관리하도록 변경
    - 유저 정보, 유저 모임 리스트 api 연동

--- front/src/app/(user)/profile/[id]/(sections)/Projects.tsx
+++ front/src/app/(user)/profile/[id]/(sections)/Projects.tsx
@@ -1,104 +1,10 @@
 "use client";
 
+import { UserGatheringInterface } from "@/types/common";
 import Template from "../components/Template";
 import useCarousel from "./useCarousel";
 
-const projects = [
-  {
-    id: "프로젝트ID",
-    name: "프로젝트 이름",
-    description: "프로젝트 설명",
-    stack: ["JavaScript", "React", "NextJs"],
-    imgUrl: "...", // 프로젝트의 대표 이미지
-    members: [
-      {
-        id: "user1",
-        nickname: "유저1_닉네임",
-        role: "frontend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-      {
-        id: "user2",
-        nickname: "유저2_닉네임",
-        role: "backend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-    ],
-  },
-  {
-    id: "프로젝트ID",
-    name: "프로젝트 이름",
-    description: "프로젝트 설명",
-    stack: ["JavaScript", "React", "NextJs"],
-    imgUrl: "...", // 프로젝트의 대표 이미지
-    members: [
-      {
-        id: "user1",
-        nickname: "유저1_닉네임",
-        role: "frontend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-      {
-        id: "user2",
-        nickname: "유저2_닉네임",
-        role: "backend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-    ],
-  },
-  {
-    id: "프로젝트ID",
-    name: "프로젝트 이름",
-    description: "프로젝트 설명",
-    stack: ["JavaScript", "React", "NextJs"],
-    imgUrl: "...", // 프로젝트의 대표 이미지
-    members: [
-      {
-        id: "user1",
-        nickname: "유저1_닉네임",
-        role: "frontend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-      {
-        id: "user2",
-        nickname: "유저2_닉네임",
-        role: "backend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-    ],
-  },
-  {
-    id: "프로젝트ID",
-    name: "프로젝트 이름",
-    description: "프로젝트 설명",
-    stack: ["JavaScript", "React", "NextJs"],
-    imgUrl: "...", // 프로젝트의 대표 이미지
-    members: [
-      {
-        id: "user1",
-        nickname: "유저1_닉네임",
-        role: "frontend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-      {
-        id: "user2",
-        nickname: "유저2_닉네임",
-        role: "backend", // 담당 역할
-        stack: ["", ""], // 기술 스택
-        imgUrl: "...", // 프로필 이미지
-      },
-    ],
-  },
-];
-
-export default function Projects() {
+export default function Projects(userProjects: UserGatheringInterface[]) {
   const { Carousel, CarouselBtns } = useCarousel();
   return (
     <Template>
@@ -106,32 +12,33 @@ export default function Projects() {
         <h3 className="text-lg font-bold">
           참여하는 프로젝트
           <span className="ml-2 font-normal text-[#8C8C8C]">
-            {projects.length}
+            {userProjects.length}
           </span>
         </h3>
         <CarouselBtns />
       </div>
       <Carousel className="rounded-lg overflow-hidden">
-        {projects.map((item, index) => {
-          return (
-            <div key={`${item}-${index}`}>
-              <div className="w-[calc(100%-20px)] h-[200px] bg-[#D0E3FF] rounded-xl p-4 flex flex-col justify-between">
-                <h5 className="font-bold mb-3">{item.name}</h5>
-                <div>{item.description}</div>
-                <div>
-                  {item.stack.map((tech, index) => (
-                    <span
-                      key={`${tech}-${index}`}
-                      className="bg-[#EEEEEE] rounded-2xl py-[2px] px-2 mr-1"
-                    >
-                      {tech}
-                    </span>
-                  ))}
+        {Array.isArray(userProjects) &&
+          userProjects.map((item, index) => {
+            return (
+              <div key={`${item}-${index}`}>
+                <div className="w-[calc(100%-20px)] h-[200px] bg-[#D0E3FF] rounded-xl p-4 flex flex-col justify-between">
+                  <h5 className="font-bold mb-3">{item.gatheringName}</h5>
+                  <div>{item.description}</div>
+                  <div>
+                    {item.techStacks.map((tech, index) => (
+                      <span
+                        key={`${tech}-${index}`}
+                        className="bg-[#EEEEEE] rounded-2xl py-[2px] px-2 mr-1"
+                      >
+                        {tech}
+                      </span>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </Carousel>
     </Template>
   );
--- front/src/app/(user)/profile/[id]/page.tsx
+++ front/src/app/(user)/profile/[id]/page.tsx
@@ -1,18 +1,35 @@
+import { getUserProfile } from "@/apis/profile";
 import Projects from "./(sections)/Projects";
-import Reviews from "./(sections)/Reviews";
+import ReviewStatistics from "./(sections)/ReviewStatistics";
 import Studies from "./(sections)/Studies";
 import UserInfo from "./(sections)/UserInfo";
 
-export default function Profile() {
+export default async function Profile() {
   // 유저 정보 받기
+  const id = 1;
+  const data = await getUserProfile(id);
+
+  const userInfo = {
+    id: data.id,
+    username: data.username,
+    nickname: data.nickname,
+    profileImage: data.profileImage,
+    temperature: data.temperature,
+    techStacks: data.techStacks,
+  };
+
+  // const userRiviews = {};
+  const userStudies = data.gatheringStudy;
+  const userProjects = data.gatheringProject || [];
+
   return (
     <div className="container mx-auto max-w-[1024px] flex flex-col gap-6 px-6 pb-10">
       <div className="grid grid-cols-2 gap-6 w-full">
-        <UserInfo />
-        <Reviews />
+        <UserInfo {...userInfo} />
+        <ReviewStatistics />
       </div>
-      <Studies />
-      <Projects />
+      <Studies {...userStudies} />
+      <Projects {...userProjects} />
     </div>
   );
 }
