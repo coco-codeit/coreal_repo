90df425acf8404b36c2995ac06adc5f781bc3d1e
황호선 <3262398@naver.com>
2024-09-24 01:24:18 +0900

    Feat: 참여중인 모임 데이터 렌더링 및 뱃지 기능 구현
    - JSON 데이터 추가 및 서버와의 연동 구현
    - JSON 서버에서 가져온 데이터 렌더링 구현
    
    - 스터디와 프로젝트를 구분하는 뱃지 기능 구현
    - 드롭다운 선택 시 뱃지 값이 고유하지 않고 변경되던 문제 수정
    - JSON 데이터에서 주최자에 따른 필터링 로직 구현 (예시 데이터 사용)
    - 신청자 정보와 연동 로직 추가

--- front/src/app/gatherings/manage/components/applied/Card.tsx
+++ front/src/app/gatherings/manage/components/applied/Card.tsx
@@ -2,46 +2,58 @@ import Image from "next/image";
 
 interface CardProps {
   title: string;
-  location: string;
+  connection: string;
   skills?: string[];
-  description?: string;
-  date: string;
-  time: string;
-  participants: string; // (현재 인원 / 최대 인원)
+  content?: string;
+  satrtDateTime: string;
+  participant: number;
+  capacity: number;
   imageUrl?: string;
   onCancel?: () => void;
+  recruitment?: { field: string; participant: number; capacity: number }[];
+  type: "study" | "project";
 }
 
 function Card({
   title,
-  location,
+  connection,
   skills,
-  description,
-  date,
-  time,
-  participants,
+  content,
+  satrtDateTime,
+  participant,
+  capacity,
   imageUrl,
   onCancel,
+  recruitment,
+  type,
 }: CardProps) {
+  const defaultImage = "/images/profile.png"; // 임시 이미지
+
   return (
-    <div className="flex flex-row h-[220px] p-6 items-start gap-6 rounded-lg bg-white border border-[#DDDCE3] mb-5">
+    <div className="flex flex-row p-6 items-start gap-6 rounded-lg bg-white border border-[#DDDCE3] mb-5">
       {/* 이미지 */}
       <div className="bg-slate-600 w-[320px] h-[172px] rounded-lg overflow-hidden relative">
-        {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt="Gathering Image"
-            layout="fill"
-            objectFit="cover"
-          />
-        ) : null}
+        <Image
+          src={imageUrl && imageUrl !== "string" ? imageUrl : defaultImage}
+          alt="image"
+          fill
+          style={{ objectFit: "cover" }}
+        />
       </div>
 
       <div className="flex flex-col flex-1">
+        <div
+          className={`px-4 py-1 mb-2 rounded-full self-start ${
+            type === "study" ? "bg-green-200" : "bg-blue-200"
+          }`}
+        >
+          {type === "study" ? "스터디" : "프로젝트"}
+        </div>
+
         <div className="flex flex-row gap-2 mb-2 text-2xl">
-          <p className="font-medium ">{title}</p>
+          <p className="font-medium">{title}</p>
           <span>|</span>
-          <span>{location}</span>
+          <span>{connection === "online" ? "온라인" : "오프라인"}</span>
         </div>
 
         {skills && (
@@ -49,7 +61,7 @@ function Card({
             {skills.map((skill, index) => (
               <span
                 key={index}
-                className="py-1 px-4 rounded-full bg-[#FFEDD5] text-[#EA580C]"
+                className="px-4 py-1 rounded-full bg-[#FFEDD5] text-[#EA580C]"
               >
                 {skill}
               </span>
@@ -57,23 +69,37 @@ function Card({
           </div>
         )}
 
-        {description && <p>{description}</p>}
+        {content && <p>{content}</p>}
 
-        <div className="flex flex-row gap-4 mb-4">
-          <span>
-            {date} · {time}
-          </span>
-          <span>{participants}</span>
+        <div className="flex flex-row gap-4 mb-2">
+          <p className="flex gap-2">
+            {`${new Date(satrtDateTime).getMonth() + 1}월 ${new Date(satrtDateTime).getDate()}일`}
+            <span>·</span>
+            {new Date(satrtDateTime).toLocaleTimeString("ko-KR", {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            })}
+          </p>
+          <span>{`${participant}/${capacity}`}</span>
         </div>
 
-        {onCancel && (
-          <button
-            className="w-[120px] p-2 flex justify-center items-center rounded-xl border border-[#EA580C] bg-white text-[#EA580C]"
-            onClick={onCancel}
-          >
-            취소하기
-          </button>
+        {recruitment && (
+          <div className="flex flex-row gap-2 mb-3">
+            {recruitment.map((role, index) => (
+              <span key={index}>
+                {role.field}: {role.participant}/{role.capacity}
+              </span>
+            ))}
+          </div>
         )}
+
+        <button
+          className="w-[120px] p-2 flex justify-center items-center rounded-xl border border-[#EA580C] bg-white text-[#EA580C]"
+          onClick={onCancel}
+        >
+          취소하기
+        </button>
       </div>
     </div>
   );
