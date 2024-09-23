852225b25e6c920c1272f5626eaad54101024341
SeanKim05 <dbgus20609@gmail.com>
2024-09-23 15:05:28 +0900

    Fix: 불필요한 라인 제거

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -104,17 +104,6 @@ const config: Config = {
           "ios-400": "#FF334B",
         },
       },
-
-      animation: {
-        "scale-in-center":
-          "scale-in-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
-      },
-      keyframes: {
-        "scale-in-center": {
-          "0%": { transform: "scale(0)", opacity: "1" },
-          "100%": { transform: "scale(1)", opacity: "1" },
-        },
-      },
     },
   },
   plugins: [],
