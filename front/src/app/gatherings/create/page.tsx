882d29d13af510ae145ff4b36c8f1815ccb85db1
srp0114 <ksy010114@gmail.com>
2024-09-24 17:45:20 +0900

    Feat: 캘린더 라이브러리 추가 및 구현 - react-day-picker, date-fns 라이브러리 추가 - 캘린더 컴포넌트 구현 - 컴포넌트 및 변수명 category에서 type으로 변경 - useFormStore 타입 수정 및 추가

--- front/src/app/gatherings/create/page.tsx
+++ front/src/app/gatherings/create/page.tsx
@@ -2,7 +2,7 @@ import GatheringForm from "@/app/gatherings/create/components/GatheringForm";
 
 function CreateGathering() {
   return (
-    <div className="max-w-[1200px] h-screen mx-auto px-10 lg:px-24">
+    <div className="max-w-[1080px] h-screen mx-auto px-10 lg:px-24">
       <GatheringForm />
     </div>
   );
