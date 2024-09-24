bb8ef154d3166a8284d4dc38e40b1a7a87e2fa82
Jungmin <miniharu.zip@gmail.com>
2024-09-24 17:33:22 +0900

    Feat: 로그인 페이지 구현 및 웹 접근성, 반응형 디자인 추가
    - PC, Table, Phone 화면 크기에 맞는 반응형 레이아웃의 회원가입 페이지를 생성
    - 입력 필드 폼 추가
    - Image 컴포넌트를 사용하여 최적화된 이미지 렌더링
    - 기존 회원을 위한 로그인 페이지로 이동하는 링크 제공

--- front/src/app/(auth)/components/SignupPrompt.tsx
+++ front/src/app/(auth)/components/SignupPrompt.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function SignUpPrompt() {
+export default function SignupPrompt() {
   return (
     <div className="flex justify-center text-xs sm:text-sm gap-x-1 sm:gap-x-[10px] mb-6">
       <p>CoReal이 처음이신가요?</p>
