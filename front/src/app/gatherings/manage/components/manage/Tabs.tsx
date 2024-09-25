dc1d3e3360a776e0dac19af9782706ba9e28fd8b
황호선 <3262398@naver.com>
2024-09-25 20:19:23 +0900

    Refactor: 드롭다운을 Headless UI 라이브러리로 수정
    - 기존 드롭다운을 Headless UI 라이브러리로 대체

--- front/src/app/gatherings/manage/components/manage/Tabs.tsx
+++ front/src/app/gatherings/manage/components/manage/Tabs.tsx
@@ -4,6 +4,9 @@ import { AppliedList } from "../applied/List";
 import { CreatedList } from "../created/List";
 import { useTabStore } from "@/store/gathers/manage/useTabStore";
 import { useDropdownStore } from "@/store/gathers/manage/useDropdownStore";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { IoIosArrowDown } from "react-icons/io";
+import { useState } from "react";
 
 const tabs: { id: "applied" | "created"; label: string }[] = [
   { id: "applied", label: "내가 참여중인 모임" },
@@ -11,14 +14,24 @@ const tabs: { id: "applied" | "created"; label: string }[] = [
 ];
 
 const dropdownOptions: { id: "all" | "study" | "project"; label: string }[] = [
-  { id: "all", label: "전체" },
-  { id: "study", label: "스터디" },
   { id: "project", label: "프로젝트" },
+  { id: "study", label: "스터디" },
+  { id: "all", label: "전체" },
 ];
 
 function Tabs() {
   const { activeTab, setActiveTab } = useTabStore();
   const { activeDropdown, setActiveDropdown } = useDropdownStore();
+
+  const [selectedLabel, setSelectedLabel] = useState("");
+
+  const handleDropdownClick = (option: {
+    id: "all" | "study" | "project";
+    label: string;
+  }) => {
+    setActiveDropdown(option.id);
+    setSelectedLabel(option.label);
+  };
 
   return (
     <div>
@@ -38,19 +51,34 @@ function Tabs() {
           ))}
         </div>
 
-        <div className="flex justify-end md:justify-start font-sans">
-          <select
-            value={activeDropdown}
-            onChange={(e) =>
-              setActiveDropdown(e.target.value as "all" | "study" | "project")
-            }
-          >
-            {dropdownOptions.map((option) => (
-              <option key={option.id} value={option.id}>
-                {option.label}
-              </option>
-            ))}
-          </select>
+        <div className="flex gap-4 items-center font-sans">
+          <Menu>
+            <p>{selectedLabel}</p>
+            <MenuButton className="flex justify-center items-center w-10 h-10 bg-gray-3 rounded-full">
+              <span>
+                <IoIosArrowDown />
+              </span>
+            </MenuButton>
+            <MenuItems
+              anchor={{
+                to: "bottom end",
+                gap: "4px",
+                offset: "-40px",
+              }}
+              className="px-4 py-2 text-display-1 w-[171px] rounded-2xl flex flex-col justify-start bg-white border border-gray-4 shadow-custom"
+            >
+              {dropdownOptions.map((option) => (
+                <MenuItem
+                  key={option.id}
+                  as="button"
+                  onClick={() => handleDropdownClick(option)}
+                  className={`text-start ${activeDropdown === option.id ? "font-bold" : ""}`}
+                >
+                  {option.label}
+                </MenuItem>
+              ))}
+            </MenuItems>
+          </Menu>
         </div>
       </div>
 
