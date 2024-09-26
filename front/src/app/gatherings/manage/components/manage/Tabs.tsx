23c34b052fa2d7cea558f8e8f00b25847b6a19d9
황호선 <3262398@naver.com>
2024-09-26 15:11:56 +0900

    WIP: 드롭다운 오류 미해결 상태로 임시 커밋
    - Headless UI 드롭다운 관련 오류 미해결
    - 추후 오류 수정 예정

--- front/src/app/gatherings/manage/components/manage/Tabs.tsx
+++ front/src/app/gatherings/manage/components/manage/Tabs.tsx
@@ -1,12 +1,8 @@
 "use client";
-
 import { AppliedList } from "../applied/List";
 import { CreatedList } from "../created/List";
 import { useTabStore } from "@/store/gathers/manage/useTabStore";
 import { useDropdownStore } from "@/store/gathers/manage/useDropdownStore";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { IoIosArrowDown } from "react-icons/io";
-import { useState } from "react";
 
 const tabs: { id: "applied" | "created"; label: string }[] = [
   { id: "applied", label: "내가 참여중인 모임" },
@@ -14,28 +10,18 @@ const tabs: { id: "applied" | "created"; label: string }[] = [
 ];
 
 const dropdownOptions: { id: "all" | "study" | "project"; label: string }[] = [
-  { id: "project", label: "프로젝트" },
-  { id: "study", label: "스터디" },
   { id: "all", label: "전체" },
+  { id: "study", label: "스터디" },
+  { id: "project", label: "프로젝트" },
 ];
 
 function Tabs() {
   const { activeTab, setActiveTab } = useTabStore();
   const { activeDropdown, setActiveDropdown } = useDropdownStore();
 
-  const [selectedLabel, setSelectedLabel] = useState("");
-
-  const handleDropdownClick = (option: {
-    id: "all" | "study" | "project";
-    label: string;
-  }) => {
-    setActiveDropdown(option.id);
-    setSelectedLabel(option.label);
-  };
-
   return (
     <div>
-      <div className="lg:text-display-1 md:text-headline text-subhead-3 h-8 flex gap-8 md:gap-0 md:flex-row flex-col justify-between md:mb-10 mb-20 text-gray-15">
+      <div className="lg:text-display-1 md:text-headline text-subhead-3 h-8 flex gap-8 md:gap-0 md:flex-row flex-col justify-between md:mb-10 mb-16 text-gray-15">
         <div className="flex justify-start gap-6">
           {tabs.map((tab) => (
             <button
@@ -51,33 +37,19 @@ function Tabs() {
           ))}
         </div>
 
-        <div className="flex lg:gap-4 gap-2 items-center font-sans justify-end lg:text-display-1 text-body-2 md:text-headline">
-          <Menu>
-            <p>{selectedLabel}</p>
-            <MenuButton className="flex justify-center items-center w-8 h-8 md:w-10 md:h-10 bg-gray-3 rounded-full">
-              <span>
-                <IoIosArrowDown />
-              </span>
-            </MenuButton>
-            <MenuItems
-              anchor={{
-                to: "bottom end",
-                offset: "-40px",
-              }}
-              className="md:gap-1 gap-2 lg:text-display-1 text-body-2 md:text-headline p-4 lg:px-4 lg:py-2 w-[111px] md:w-[171px] rounded-2xl flex flex-col justify-start bg-white border border-gray-4 shadow-custom"
-            >
-              {dropdownOptions.map((option) => (
-                <MenuItem
-                  key={option.id}
-                  as="button"
-                  onClick={() => handleDropdownClick(option)}
-                  className={`text-start ${activeDropdown === option.id ? "font-title" : ""}`}
-                >
-                  {option.label}
-                </MenuItem>
-              ))}
-            </MenuItems>
-          </Menu>
+        <div className="flex justify-end md:justify-start font-sans">
+          <select
+            value={activeDropdown}
+            onChange={(e) =>
+              setActiveDropdown(e.target.value as "all" | "study" | "project")
+            }
+          >
+            {dropdownOptions.map((option) => (
+              <option key={option.id} value={option.id}>
+                {option.label}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
 
@@ -88,5 +60,4 @@ function Tabs() {
     </div>
   );
 }
-
 export { Tabs as GatheringTabs };
