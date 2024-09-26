38951386486251e29d094336d1d196f530b9497a
Daniel LEE <dmsgur7112@gmail.com>
2024-09-26 11:09:42 +0900

    Fix: 버그 해결
    - 모달 내부 클릭 시 모달이 꺼지는 문제 해결
    - TailwindCSS가 입혀지지 않는 문제 해결
    - 모달 컴포넌트 추상화 작업 진행

--- front/src/hooks/useModal.tsx
+++ front/src/hooks/useModal.tsx
@@ -1,6 +1,36 @@
 import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
+interface InlineStyleInterface {
+  [style: string]: string | number;
+}
+
+// tailwindCSS가 특정 조건에서 CSS가 입혀지지 않는 문제가 있어
+// 아래와 같이 인라인 스타일을 지정해서 적용해두었습니다.
+// 작성자: 이은혁
+
+const bgStyle: InlineStyleInterface = {
+  position: "fixed",
+  inset: 0,
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "rgb(0 0 0 / 0.5)",
+  WebkitBackdropFilter:
+    "blur(16px) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia)",
+  backdropFilter:
+    "blur(16px) var(--tw-backdrop-brightness) var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale) var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert) var(--tw-backdrop-opacity) var(--tw-backdrop-saturate) var(--tw-backdrop-sepia)",
+};
+
+const modalStyle: InlineStyleInterface = {
+  minWidth: "150px",
+  minHeight: "200px",
+  padding: "1rem",
+};
+
 /**
  * 모달을 쉽게 제작하기 위한 훅으로,
  * Modal 컴포넌트와 모달의 상태를 관리하는 open, setOpen이 제공됩니다.
@@ -12,11 +42,8 @@ import { createPortal } from "react-dom";
  *   <>
  *     <button onClick={() => setOpen(true)}>모달 띄우기</button>
  *     <Modal>
- *       <Modal.Background className="bg-black/50" />
- *       <Modal.Container>
- *         <h2>모달 컨텐츠</h2>
- *         <button onClick={() => setOpen(false)}>모달 닫기</button>
- *       </Modal.Container>
+ *       <h2>모달 컨텐츠</h2>
+ *       <button onClick={() => setOpen(false)}>모달 닫기</button>
  *     </Modal>
  *   </>
  * );
@@ -33,62 +60,28 @@ export default function useModal() {
     className?: string;
     children?: React.ReactNode;
   }) {
+    const modalOverlayRef = useRef(null);
+
+    const handleClickOverlay = (e: React.MouseEvent) => {
+      if (e.target === modalOverlayRef.current) setOpen(false);
+    };
+
     return (
       <Portal>
         {open && (
-          <div className={`fixed top-0 left-0 ${className ? className : ""}`}>
-            {children}
+          <div
+            ref={modalOverlayRef}
+            onClick={handleClickOverlay}
+            style={bgStyle}
+          >
+            <div className={className} style={modalStyle}>
+              {children}
+            </div>
           </div>
         )}
       </Portal>
     );
   }
-
-  function Background({
-    className,
-    children,
-  }: {
-    className?: string;
-    children?: React.ReactNode;
-  }) {
-    return (
-      <div
-        className={`fixed top-0 left-0 w-full h-full ${className ? className : ""}`}
-      >
-        {children}
-      </div>
-    );
-  }
-
-  function Container({
-    className,
-    children,
-  }: {
-    className?: string;
-    children?: React.ReactNode;
-  }) {
-    const containerRef = useRef(null);
-
-    const handleClickOutside = (e: React.MouseEvent) => {
-      if (e.target !== containerRef.current) setOpen(false);
-    };
-
-    return (
-      <div
-        className="absolute top-0 left-0 w-screen h-screen flex flex-col justify-center items-center"
-        onClick={handleClickOutside}
-      >
-        <div
-          ref={containerRef}
-          className={`relative rounded-xl bg-white p-2 ${className ? className : ""}`}
-        >
-          {children}
-        </div>
-      </div>
-    );
-  }
-  Modal.Background = Background;
-  Modal.Container = Container;
 
   return { Modal, open, setOpen };
 }
