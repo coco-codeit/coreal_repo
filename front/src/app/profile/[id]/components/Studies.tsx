39ed07e10193c0df063209cdf10cde5304ddc890
Daniel LEE <dmsgur7112@gmail.com>
2024-09-26 17:55:43 +0900

    Feat: 리뷰 관리, 프로필 수정 버튼 추가

--- front/src/app/profile/[id]/components/Studies.tsx
+++ front/src/app/profile/[id]/components/Studies.tsx
@@ -4,10 +4,17 @@ import { UserGatheringInterface } from "@/types/common";
 import Template from "../components/Template";
 import useCarousel from "./useCarousel";
 import Card from "./Card";
+import Link from "next/link";
 
-export default function Studies(userStudies: UserGatheringInterface[]) {
+export default function Studies({
+  id,
+  studies,
+}: {
+  id: number;
+  studies: UserGatheringInterface[];
+}) {
   const { Carousel, CarouselBtns } = useCarousel({
-    length: userStudies.length,
+    length: studies.length,
   });
   return (
     <Template>
@@ -15,14 +22,19 @@ export default function Studies(userStudies: UserGatheringInterface[]) {
         <h3 className="text-base md:text-lg font-bold">
           참여하는 스터디
           <span className="ml-2 font-normal text-[#8C8C8C]">
-            {userStudies.length}
+            {studies.length}
           </span>
         </h3>
-        <CarouselBtns />
+        <div>
+          <Link href={`./${id}/reviews`} className="hover:font-bold">
+            리뷰 관리
+          </Link>
+          <CarouselBtns />
+        </div>
       </div>
-      {Array.isArray(userStudies) && userStudies.length > 0 && (
+      {Array.isArray(studies) && studies.length > 0 && (
         <Carousel className="rounded-lg overflow-hidden">
-          {userStudies.map((item, index) => (
+          {studies.map((item, index) => (
             <Card
               key={`${item}-${index}`}
               item={item}
@@ -31,7 +43,7 @@ export default function Studies(userStudies: UserGatheringInterface[]) {
           ))}
         </Carousel>
       )}
-      {(!Array.isArray(userStudies) || userStudies.length === 0) && (
+      {(!Array.isArray(studies) || studies.length === 0) && (
         <div className="py-10 text-center">아직 참여중인 스터디가 없어요</div>
       )}
     </Template>
--- front/src/app/profile/[id]/page.tsx
+++ front/src/app/profile/[id]/page.tsx
@@ -1,12 +1,12 @@
-import { getUserProfile } from "@/apis/profile";
 import Projects from "./components/Projects";
 import ReviewStatistics from "./components/ReviewStatistics";
 import Studies from "./components/Studies";
 import UserInfo from "./components/UserInfo";
+import { getUserProfile } from "@/apis/profile";
 
-export default async function Profile() {
+export default async function Profile({ params }: { params: { id: string } }) {
   // 유저 정보 받기
-  const id = 1;
+  const id = parseInt(params.id);
   const data = await getUserProfile(id);
 
   const userInfo = {
@@ -19,17 +19,17 @@ export default async function Profile() {
   };
 
   // const userRiviews = {};
-  const userStudies = data.gatheringStudy;
+  const userStudies = data.gatheringStudy || [];
   const userProjects = data.gatheringProject || [];
 
   return (
     <div className="container mx-auto w-full sm:w-[500px] md:w-[700px] lg:w-full max-w-[1024px] flex flex-col gap-3 md:gap-6 px-3 pb-6 md:px-6 md:pb-10 text-sm md:text-base">
       <div className="grid lg:grid-cols-2 gap-3 md:gap-6">
-        <UserInfo {...userInfo} />
+        <UserInfo info={userInfo} id={id} />
         <ReviewStatistics />
       </div>
-      <Studies {...userStudies} />
-      <Projects {...userProjects} />
+      <Studies studies={userStudies} id={id} />
+      <Projects projects={userProjects} id={id} />
     </div>
   );
 }
