c4e51a785babce08b993bb4387c7995dfcd59c29
Daniel LEE <dmsgur7112@gmail.com>
2024-09-30 11:49:12 +0900

    Feat: 내 프로필 레이아웃 구현

---
+++ front/src/app/mypage/components/my-profile/MyProfile.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import ModifyProfile from "./ModifyProfile";
+export default function MyProfile() {
+  return (
+    <div>
+      <h1>내 프로필</h1>
+      <ModifyProfile />
+      <div>
+        <Image src="" alt="" />
+        <p>Nickname</p>
+        <p>company: </p>
+        <p>E-mail: </p>
+      </div>
+    </div>
+  );
+}
---
+++ front/src/app/mypage/page.tsx
@@ -0,0 +1,12 @@
+import ManageTab from "./components/(manage-tab)/ManageTab";
+import MyProfile from "./components/(my-profile)/MyProfile";
+
+export default function MyPage() {
+  return (
+    <div>
+      <h1>마이 페이지</h1>
+      <MyProfile />
+      <ManageTab />
+    </div>
+  );
+}
