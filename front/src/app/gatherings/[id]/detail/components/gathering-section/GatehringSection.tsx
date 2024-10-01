b65a0ab28936d5794555d8f541afab5c1753f870
SeanKim05 <dbgus20609@gmail.com>
2024-10-01 18:31:42 +0900

    Design: 모임상세 반응형 적용 진행

--- front/src/app/gatherings/[id]/detail/components/gathering-section/GatehringSection.tsx
+++ front/src/app/gatherings/[id]/detail/components/gathering-section/GatehringSection.tsx
@@ -7,8 +7,8 @@ import GatheringInfo from "./GatheringInfo";
 
 export default function GatehringSection() {
   return (
-    <section className="flex h-[270px] gap-6">
-      <div className="relative w-1/2">
+    <section className="flex items-center justify-center md:flex-row flex-col gap-6">
+      <div className="relative w-full md:w-1/2 h-[270px]">
         <Image
           className="relative rounded-3xl"
           src="/images/detail/gatherDetail.png"
@@ -18,7 +18,7 @@ export default function GatehringSection() {
         <DeadLineTag endTime="2024-07-25T09:06:16.184Z" />
       </div>
 
-      <div className="w-1/2 py-6 rounded-[24px] border-2 border-gray-200">
+      <div className="w-full md:w-1/2 h-[270px] py-6 rounded-[24px] border-2 border-gray-200">
         <GatheringInfo />
 
         <hr className="border-dashed border-gray-400 mt-[43px]" />
--- front/src/app/gatherings/[id]/detail/components/gathering-section/UserAvatar.tsx
+++ front/src/app/gatherings/[id]/detail/components/gathering-section/UserAvatar.tsx
@@ -5,7 +5,7 @@ export default function UserAvatar() {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center">
-        <div className="">모집정원 16명</div>
+        <div className="text-[14px]">모집정원 16명</div>
         <div className="flex ml-[22px]">
           <div className="h-[29px] w-[29px] rounded-full bg-gray-300 -ml-[10px]"></div>
           <div className="h-[29px] w-[29px] rounded-full bg-gray-300 -ml-[10px]"></div>
