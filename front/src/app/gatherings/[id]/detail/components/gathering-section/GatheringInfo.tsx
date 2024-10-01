f06c3efd23bdbc242ad4fdedda270df7d74f72c4
SeanKim05 <dbgus20609@gmail.com>
2024-10-01 18:31:42 +0900

    Design: 모임상세 반응형 적용 진행

--- front/src/app/gatherings/[id]/detail/components/gathering-section/GatheringInfo.tsx
+++ front/src/app/gatherings/[id]/detail/components/gathering-section/GatheringInfo.tsx
@@ -10,7 +10,7 @@ export default function GatheringInfo() {
         <LikeButton />
       </div>
       <h2>달램핏 오피스 스트레칭</h2>
-      <span>을지로 3가 서울시 중구 청계천로 100</span>
+      <span className="text-[14px]">을지로 3가 서울시 중구 청계천로 100</span>
       <div className="flex mt-3">
         <DateTag dateText="8월 7일" textColor="white" />
         <DateTag dateText="17:30" textColor="#EA580C" />
