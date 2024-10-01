3f0000608d584676917169b78c94cab9f625518d
srp0114 <ksy010114@gmail.com>
2024-10-01 23:34:11 +0900

    Feat: 카드 컴포넌트 UI 구현 - 카드 컴포넌트 UI 구현 - 개설확정 아이콘 잘림 현상 수정

--- front/src/app/gatherings/components/ConfirmBadge.tsx
+++ front/src/app/gatherings/components/ConfirmBadge.tsx
@@ -2,9 +2,13 @@ import { FaCircleCheck } from "react-icons/fa6";
 
 function ConfirmBadge() {
   return (
-    <div className="flex flex-row justify-start items-center w-[77px] h-[24px] gap-1 ">
-      <FaCircleCheck className="w-6 h-6 text-orange-500" />
-      <p className="text-body-1 text-orange-500">개설확정</p>
+    <div className="flex flex-row items-center gap-1">
+      <div className="flex items-center justify-center w-6 h-6">
+        <FaCircleCheck className="text-orange-500 w-[18px] h-[18px]" />
+      </div>
+      <span className="text-body-1 text-orange-500 leading-none pt-[2px]">
+        개설확정
+      </span>
     </div>
   );
 }
--- front/src/app/gatherings/list/components/Card.tsx
+++ front/src/app/gatherings/list/components/Card.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import { format } from "date-fns";
+import { BsPersonFill } from "react-icons/bs";
+import { LuArrowRight } from "react-icons/lu";
 import { Gatherings } from "@/app/types/gatherings";
-import ConfirmBadge from "@/app/gatherings/components/ConfirmBadge";
 import LikeButton from "@/app/gatherings/components/LikeButton";
+import DateTag from "@/app/gatherings/components/DateTag";
+import ConfirmBadge from "@/app/gatherings/components/ConfirmBadge";
+import ProgressBar from "@/app/gatherings/components/ProgressBar";
 
 interface CardProps {
   data: Gatherings[];
@@ -11,30 +16,76 @@ function Card({ data }: CardProps) {
   return (
     <div className="grid grid-cols-1 gap-6 py-6 w-full">
       {data.length > 0
-        ? data.map((item) => (
-            <div
-              key={item.id}
-              className="h-[316px] md:h-[156px] grid grid-rows-[156px_1fr] md:grid-cols-[280px_1fr]"
-            >
-              <div className="relative w-full rounded-t-3xl md:rounded-tr-none md:rounded-l-3xl overflow-hidden">
-                <Image
-                  src={item.image}
-                  alt={`${item.name} 대표이미지`}
-                  fill={true}
-                  className="object-cover"
-                />
-              </div>
-              <div className="flex flex-col border-2 border-gray-900 rounded-b-3xl md:rounded-b-none md:rounded-tr-3xl md:rounded-br-3xl">
-                <div className="flex space-between">
-                  <p>{item.name}</p>
-                  <LikeButton />
+        ? data.map((item) => {
+            // 월/일, 시/분을 각각 포맷
+            const formattedDate = format(new Date(item.dateTime), "M월 dd일");
+            const formattedTime = format(new Date(item.dateTime), "HH:mm");
+
+            return (
+              <div
+                key={item.id}
+                className="h-[316px] md:h-[156px] grid grid-rows-[156px_1fr] md:grid-cols-[280px_1fr]"
+              >
+                <div className="relative w-full rounded-t-3xl md:rounded-tr-none md:rounded-l-3xl overflow-hidden">
+                  <Image
+                    src={item.image}
+                    alt={`${item.name} 대표이미지`}
+                    fill={true}
+                    className="object-cover"
+                  />
                 </div>
-                <div>
-                  <ConfirmBadge />
+
+                <div className="flex flex-col border-2 border-gray-100 py-4 px-4 md:pl-6 rounded-b-3xl md:rounded-b-none md:rounded-tr-3xl md:rounded-br-3xl">
+                  <div className="flex justify-between mb-[20px] md:mb-[21px]">
+                    <div className="flex flex-col gap-2">
+                      <div className="flex flex-row justify-start items-center gap-2">
+                        <span className="text-lg text-gray-800">
+                          {item.name}
+                        </span>
+                        <span className="text-lg text-gray-900">|</span>
+                        <span className="text-body-1 text-gray-700">
+                          {item.location}
+                        </span>
+                      </div>
+                      <div className="flex">
+                        <DateTag dateText={formattedDate} textColor="#EA580C" />
+                        <DateTag dateText={formattedTime} textColor="white" />
+                      </div>
+                    </div>
+                    <LikeButton />
+                  </div>
+
+                  <div className="flex justify-between md:pr-2 items-end mt-2">
+                    <div className="grid w-full max-w-[197px] md:max-w-[255px] lg:max-w-[556px] ">
+                      <div className="flex flex-row gap-2 items-center">
+                        <div className="flex flex-row gap-[2px] py-[2px] h-[20px]">
+                          <BsPersonFill className="w-4 h-4 text-gray-700" />
+                          <span className="text-body-1 leading-[18px] text-gray-700">
+                            {item.participantCount}/{item.capacity}
+                          </span>
+                        </div>
+                        {item.participantCount >= 5 && <ConfirmBadge />}
+                      </div>
+
+                      <div className="flex items-start -mt-1 -mb-2">
+                        <ProgressBar
+                          percent={
+                            (item.participantCount / item.capacity) * 100
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div className="flex items-end gap-2 h-6">
+                      <span className="text-orange-600 leading-none">
+                        join now
+                      </span>
+                      <LuArrowRight className="text-orange-600 w-[18px] h-[18px]" />
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
+            );
+          })
         : null}
     </div>
   );
