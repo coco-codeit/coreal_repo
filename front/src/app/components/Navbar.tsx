4aedf8c9459db05300cc7ced7820d0659faf2996
황호선 <3262398@naver.com>
2024-10-02 13:58:30 +0900

    Design: 반응형 레이아웃 구현 및 텍스트 활성화 색상 적용
    - 모바일, 태블릿, 데스크탑 화면 크기에 맞게 반응형 레이아웃 구현
    - 텍스트 활성화 시 색상 적용

--- front/src/app/components/Navbar.tsx
+++ front/src/app/components/Navbar.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const pathname = usePathname();
 
   const toggleLogin = () => {
     setIsLoggedIn(!isLoggedIn);
   };
 
+  const isActive = (path: string) =>
+    pathname === path || (path === "/" && pathname === "/gatherings")
+      ? "text-[#111827]"
+      : "text-[#FFF7ED]";
+
   return (
-    <nav className="container max-w-[1920px] py-[18px] bg-[#EA580C] text-white">
-      <div className="flex justify-around items-center">
-        <section className="flex gap-5 items-center">
-          <div>
+    <nav className="container max-w-[1920px] h-14 md:max-h-[60px] bg-[rgb(234,88,12)] text-white border-b-2 border-[#111827]">
+      <div className="flex h-full lg:justify-around items-center md:px-6 justify-between px-4">
+        <section className="flex md:gap-5 gap-3 items-center">
+          <div className="hidden md:block">
             <Link href="/" aria-label="Go to homepage">
               <Image
                 width={73}
@@ -25,16 +32,32 @@ export default function Navbar() {
               />
             </Link>
           </div>
+          <div className="md:hidden">
+            <Link href="/" aria-label="Go to homepage">
+              <Image
+                width={56}
+                height={27}
+                alt="Site Logo Text"
+                src="/images/logo-text-small.svg"
+              />
+            </Link>
+          </div>
 
-          <div className="flex mt-[2px] gap-6">
+          <div className="flex pt-[2px] md:gap-6 gap-3">
             <div>
-              <Link href="/">모임 찾기</Link>
+              <Link href="/" className={isActive("/")}>
+                모임 찾기
+              </Link>
             </div>
             <div>
-              <Link href="/#">찜한 모임</Link>
+              <Link href="/#" className={isActive("/#")}>
+                찜한 모임
+              </Link>
             </div>
             <div>
-              <Link href="/#">모든 리뷰</Link>
+              <Link href="/reviews" className={isActive("/reviews")}>
+                모든 리뷰
+              </Link>
             </div>
           </div>
         </section>
@@ -42,20 +65,18 @@ export default function Navbar() {
         <section>
           {!isLoggedIn ? (
             <>
-              <Link href="/#" onClick={toggleLogin}>
+              <Link href="/#" onClick={toggleLogin} className={isActive("/#")}>
                 로그인
               </Link>
             </>
           ) : (
             <div className="relative">
-              <Link href="/#" aria-label="Go to profile">
                 <Image
                   width={40}
                   height={40}
-                  alt="user profile"
+                  alt="profile"
                   src="/images/profile.svg"
                 />
-              </Link>
             </div>
           )}
         </section>
