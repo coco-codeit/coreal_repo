d5fcdcdf1434597b694152d5a2143b832b85bd13
SeanKim05 <dbgus20609@gmail.com>
2024-10-04 14:35:35 +0900

    Comment: axios 에러 방지 주석해제

--- front/src/apis/index.ts
+++ front/src/apis/index.ts
@@ -1,13 +1,13 @@
 // !deprecated) libs 폴더에 있는 axiosInstance로 사용해 주세요!
 
-// import axios from "axios";
+import axios from "axios";
 
-// const getApiHost = () => {
-//   const api = "https://fe-adv-project-together-dallaem.vercel.app/1";
-//   return api;
-// };
-// const api = axios.create({
-//   baseURL: getApiHost(),
-// });
+const getApiHost = () => {
+  const api = "https://fe-adv-project-together-dallaem.vercel.app/1";
+  return api;
+};
+const api = axios.create({
+  baseURL: getApiHost(),
+});
 
-// export default api;
+export default api;
