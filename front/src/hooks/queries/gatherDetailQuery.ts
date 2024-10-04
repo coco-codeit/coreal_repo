166b5ac55b4d4dbf8330ca77a6446f37ce44dab0
SeanKim05 <dbgus20609@gmail.com>
2024-10-04 14:04:14 +0900

    Feat: react-query 추가

--- front/src/hooks/queries/gatherDetailQuery.ts
+++ front/src/hooks/queries/gatherDetailQuery.ts
@@ -1,5 +1,9 @@
-import { getGatherDetail } from "@/libs/gatherDetail";
-import { useQuery } from "@tanstack/react-query";
+import {
+  getGatherDetail,
+  postJoinGather,
+  putCancelGather,
+} from "@/libs/gatherDetail";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useGatherDeatilQuery = (gatherId: string) => {
   return useQuery({
@@ -7,3 +11,49 @@ export const useGatherDeatilQuery = (gatherId: string) => {
     queryFn: () => getGatherDetail(gatherId),
   });
 };
+
+export const useGatherParticipants = (gatherId: string) => {
+  return useQuery({
+    queryKey: ["gatherParticipants"],
+    queryFn: () => getGatherDetail(gatherId),
+  });
+};
+
+//공통으로 사용가능한지 검토
+const useGatherMutation = (
+  gatherId: string,
+  mutationFn: (gatherId: string) => Promise<unknown>,
+) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: () => mutationFn(gatherId),
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ["gatherDetail"] }),
+  });
+};
+
+export const useGatherJoinMutation = (gatherId: string) => {
+  return useGatherMutation(gatherId, postJoinGather);
+};
+
+export const useGatherCancelMutation = (gatherId: string) => {
+  return useGatherMutation(gatherId, putCancelGather);
+};
+
+// export const useGatherJoinMutaiton = (gatherId: string) => {
+//   const queryClient = useQueryClient();
+//   useMutation({
+//     mutationFn: () => postJoinGather(gatherId),
+//     onSuccess: () =>
+//       queryClient.invalidateQueries({ queryKey: ["gatherDetail"] }),
+//   });
+// };
+
+// export const useGatherCancelMutation = (gatherId: string) => {
+//   const queryClient = useQueryClient();
+//   useMutation({
+//     mutationFn: () => putCancelGather(gatherId),
+//     onSuccess: () =>
+//       queryClient.invalidateQueries({ queryKey: ["gatherDetail"] }),
+//   });
+// };
--- front/src/libs/gatherDetail.ts
+++ front/src/libs/gatherDetail.ts
@@ -5,3 +5,21 @@ export const getGatherDetail = async (gatherId: string) => {
 
   return res?.data;
 };
+
+export const putCancelGather = async (gatherId: string) => {
+  const res = await api.put(`gathering/${gatherId}/cancel`);
+
+  return res?.data;
+};
+
+export const postJoinGather = async (gatherId: string) => {
+  const res = await api.post(`gathering/${gatherId}/join`);
+
+  return res?.data;
+};
+
+export const getGatherParticipants = async (gatherId: string) => {
+  const res = await api.get(`gathering/${gatherId}/partipants`);
+
+  return res?.data;
+};
