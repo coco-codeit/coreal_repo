b1ed39c53129228dfcf6a449cd3a5dfb00e1d554
SeanKim05 <dbgus20609@gmail.com>
2024-10-04 09:12:48 +0900

    Design: 하단 버튼영역 레이아웃 작업

--- front/src/app/gatherings/[id]/detail/page.tsx
+++ front/src/app/gatherings/[id]/detail/page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import GatehringSection from "./components/gathering-section/GatehringSection";
 import CommentsSection from "./components/comments-section/CommentsSection";
+import ActionBtnGroup from "./components/comments-section/ActionBtnGroup";
 
 export default function detail() {
   return (
     <div className="h-screen-minus-nav bg-[#F3F4F6]">
-      <div className="min-h-screen-minus-nav mx-auto container max-w-[1200px] px-6 md:px-[102px] pt-10 bg-white">
+      <div className="mx-auto container max-w-[1200px] px-6 md:px-[102px] pt-10 bg-white">
         <GatehringSection />
         <CommentsSection />
       </div>
+      <ActionBtnGroup />
     </div>
   );
 }
