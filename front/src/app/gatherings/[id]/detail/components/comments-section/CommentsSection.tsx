400b9ccfa3b9c7e853ce2566343ddaeeae252fa9
SeanKim05 <dbgus20609@gmail.com>
2024-10-07 11:37:55 +0900

    Feat: Comment Section 유저정보 API 연동

--- front/src/app/gatherings/[id]/detail/components/comments-section/CommentsSection.tsx
+++ front/src/app/gatherings/[id]/detail/components/comments-section/CommentsSection.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import CommentsCard from "./CommentsCard";
 import Pagination from "./Pagination";
 import { useReviews } from "@/hooks/reviews/useReviews";
+import LoadingSpinner from "@/app/components/LoadingSpinner";
 
 const itemsPerPage = 4;
 const totalItems = 100;
@@ -12,10 +13,9 @@ export default function CommentsSection({ pageId }: { pageId: string }) {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { data: reviewData } = useReviews({
+  const { data: reviewData, isLoading: isReviewLoading } = useReviews({
     gatherId: pageId,
   });
-  console.log(reviewData);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -23,20 +23,30 @@ export default function CommentsSection({ pageId }: { pageId: string }) {
 
   return (
     <section className="mt-6 p-6 border-t-2 border-[#E5E7EB]">
-      <h2 className="text-[18px] font-semibold mb-[16px]">
-        이용자들은 이 프로그램을 이렇게 느꼈어요!
-      </h2>
-      <CommentsCard />
-      <CommentsCard />
-      <CommentsCard />
-      <CommentsCard />
-      <div className="mt-2 mb-[6px]">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
-      </div>
+      {isReviewLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          <div className="min-h-[500px]">
+            <h2 className="text-[18px] font-semibold mb-[16px]">
+              이용자들은 이 프로그램을 이렇게 느꼈어요!
+            </h2>
+            {reviewData?.map((_, idx) => (
+              <CommentsCard
+                key={reviewData[idx].id}
+                singleReviewData={reviewData[idx]}
+              />
+            ))}
+          </div>
+          <div className="mt-2 mb-[6px]">
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        </>
+      )}
     </section>
   );
 }
