1bdead95aa0f3b5be34d4189b99b2efd7ac6e275
SeanKim05 <dbgus20609@gmail.com>
2024-10-11 13:31:55 +0900

    Fix: 이미지 사이즈 속성 사이즈변경

--- front/src/app/gatherings/[id]/detail/components/gathering-section/GatehringSection.tsx
+++ front/src/app/gatherings/[id]/detail/components/gathering-section/GatehringSection.tsx
@@ -34,7 +34,7 @@ export default function GatehringSection({
           className="relative rounded-3xl"
           src={detailData.image || "/images/default_gathering_image.png"}
           alt="Gather Detail Img"
-          sizes="100vw"
+          sizes="100%"
           fill
         />
         <DeadLineTag endTime={detailData?.registrationEnd} type="lg" />
