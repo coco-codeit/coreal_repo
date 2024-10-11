de3a95137fac01b9b312db60cc3705aff32b848d
SeanKim05 <dbgus20609@gmail.com>
2024-10-11 09:32:30 +0900

    Fix: 상세데이터 구조 변경

--- front/src/app/gatherings/[id]/detail/page.tsx
+++ front/src/app/gatherings/[id]/detail/page.tsx
@@ -10,12 +10,17 @@ import { useGatherDeatilQuery } from "@/hooks/queries/gatherDetailQuery";
 
 export default function Detail() {
   const { id }: { id: string } = useParams();
-  const { data: detailData } = useGatherDeatilQuery(id);
-
+  const { data: detailData, isLoading: isDetailLoading } =
+    useGatherDeatilQuery(id);
+  console.log(detailData);
   return (
     <div className="h-screen-minus-nav bg-[#F3F4F6]">
       <div className="mx-auto container max-w-[1200px] px-6 md:px-[102px] pt-10 bg-white">
-        <GatehringSection pageId={id} />
+        <GatehringSection
+          pageId={id}
+          detailData={detailData}
+          isDetailLoading={isDetailLoading}
+        />
         <CommentsSection pageId={id} />
       </div>
       <ActionBtnGroup pageId={id} createdBy={detailData?.createdBy} />
