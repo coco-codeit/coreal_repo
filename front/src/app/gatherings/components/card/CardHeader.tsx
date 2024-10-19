070c85940205eb7e79521ae4fba669426888b47d
srp0114 <ksy010114@gmail.com>
2024-10-19 17:03:03 +0900

    Fix: 모임 장소 텍스트 공간 확보 및 제목 텍스트 자르기 추가

--- front/src/app/gatherings/components/card/CardHeader.tsx
+++ front/src/app/gatherings/components/card/CardHeader.tsx
@@ -14,11 +14,15 @@ function CardHeader({
 }) {
   return (
     <div className="flex justify-between mb-[20px] md:mb-[21px]">
-      <div className="flex flex-col gap-2">
-        <div className="flex flex-row justify-start items-center gap-2">
-          <span className="text-lg font-semibold text-gray-800">{name}</span>
+      <div className="flex flex-col gap-2 pr-[15px]">
+        <div className="flex flex-row justify-start items-center gap-2 max-w-[248px] md:max-w-[305px] lg:max-w-full">
+          <span className="text-lg font-semibold text-gray-800 truncate">
+            {name}
+          </span>
           <span className="text-lg font-semibold text-gray-900">|</span>
-          <span className="text-sm text-gray-700">{location}</span>
+          <span className="text-sm text-gray-700 max-w-[60px] flex-shrink-0">
+            {location}
+          </span>
         </div>
         <div className="flex">
           <DateTag dateText={dateTime} textColor={"white"} type={"day"} />
