969d6d29982ca0d19092a2f9fbc02678fc370e83
SeanKim05 <dbgus20609@gmail.com>
2025-02-18 15:12:04 +0900

    Refactor: 불필요한 바운더리 삭제

--- front/src/app/gatherings/[id]/detail/page.tsx
+++ front/src/app/gatherings/[id]/detail/page.tsx
@@ -1,25 +1,29 @@
 import CommentsSection from "../../components/detail/comments-section/CommentsSection";
-import GatherInfo from "../../components/detail/GatherInfo";
+import GatheringSection from "../../components/detail/gathering-section/GatehringSection";
 
 import {
   HydrationBoundary,
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
-import { prefetchGatherReview } from "@/hooks/queries/gatherDetailQuery";
+import {
+  prefetchGatherReview,
+  prefetchGatherDetailQuery,
+} from "@/hooks/queries/gatherDetailQuery";
 
 export default async function Detail({ params }: { params: { id: number } }) {
   const queryClient = new QueryClient();
   const { id } = params;
 
   await prefetchGatherReview(queryClient, { pageId: id, offset: 0, limit: 4 });
+  await prefetchGatherDetailQuery(queryClient, id);
 
   const dehydratedState = dehydrate(queryClient);
 
   return (
     <div className="min-h-screen-minus-nav mx-auto container max-w-[1200px] px-6 md:px-[102px] pt-10 bg-white">
-      <GatherInfo pageId={id} />
       <HydrationBoundary state={dehydratedState}>
+        <GatheringSection pageId={id} />
         <CommentsSection pageId={id} />
       </HydrationBoundary>
     </div>
